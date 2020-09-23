83eecc06f03cb1ec55708774c72343c1856a259b
Cheolkie Kim <ckiekim@naver.com>
2020-09-23 11:09:51 +0900

    문제풀이 및 리커전

---
+++ 05.함수/12.피타고라스.js
@@ -0,0 +1,19 @@
+// 연습문제 - 2
+
+let a, b, c;
+let outerBreak = false;
+for (a=1; a<=332; a++) {
+    for (b=a+1; b<=499; b++) {
+        c = 1000 - a - b;
+        if (c > (a + b))
+            continue;
+        if (c*c === (a*a + b*b)) {
+            console.log(a, b, c);
+            console.log(a*a, b*b, c*c);
+            outerBreak = true;
+            break;
+        }
+    }
+    if (outerBreak)
+        break;
+}
---
+++ 05.함수/13.digitalClock.js
@@ -0,0 +1,12 @@
+// 연습문제 - 3
+
+let total = 0;
+for (let hour=0; hour<=23; hour++) {
+    for (let min=0; min<60; min++) {
+        let time = hour + ':' + min;
+        if (time.indexOf('3') >= 0)
+            total += 60;
+    }
+}
+
+console.log(total);
