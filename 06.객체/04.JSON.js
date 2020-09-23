b84f0354fbab8809ef526691740269089a84c776
Cheolkie Kim <ckiekim@naver.com>
2020-09-23 13:59:59 +0900

    객체

---
+++ 06.객체/04.JSON.js
@@ -0,0 +1,12 @@
+let persons = [
+    {name: '홍길동', gender: '남자', age: 29},
+    {name: '성춘향', gender: '여자', age: 17}
+];
+
+let externalForm = JSON.stringify(persons);
+console.log(externalForm);
+
+let p = JSON.parse(externalForm);
+for (let person of p) {
+    console.log(person.name, person.gender, person.age);
+}
