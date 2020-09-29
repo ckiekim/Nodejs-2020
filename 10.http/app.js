73e89a1fbbb3c1c989f54e6caec94c282a0a2a0b
Cheolkie Kim <ckiekim@naver.com>
2020-09-29 10:55:43 +0900

    App 조회

--- 10.http/app.js
+++ 10.http/app.js
@@ -3,23 +3,37 @@ const url = require('url');
 const qs = require('querystring');
 const fs = require('fs');
 const view = require('./view/index');
+const template = require('./view/template');
 
 http.createServer(function(req, res) {
     let pathname = url.parse(req.url).pathname;
+    let query = url.parse(req.url, true).query;
+    console.log(pathname, query.id);
     switch(pathname) {
     case '/':
-        fs.readdir('data', function(error, filelist) {
-            let list = '';
-            for (let file of filelist) {
-                let filename = file.substring(0, file.length-4);
-                list += `<li><a href="/?id=${filename}">${filename}</a></li>`;
-            }
-            let html = view.index(list);
-            res.end(html);
-        });
+        if (query.id === undefined) {
+            fs.readdir('data', function(error, filelist) {
+                let list = template.listGen(filelist);
+                let content = template.HOME_CONTENTS;
+                let control = template.buttonGen();
+                let html = view.index('Web 기술', list, content, control);
+                res.end(html);
+            });
+        } else {
+            fs.readdir('data', function(error, filelist) {
+                let list = template.listGen(filelist);
+                let title = query.id;
+                let control = template.buttonGen(title);
+                let filename = 'data/' + title + '.txt';
+                fs.readFile(filename, 'utf8', (error, buffer) => {
+                    let html = view.index(title, list, buffer, control);
+                    res.end(html);
+                });
+            });
+        }
         break;
     default:
-        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.writeHead(404);
         res.end();
     }
 }).listen(3000, () => {
